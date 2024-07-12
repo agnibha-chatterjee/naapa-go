66bf5dcea81ed768c629591c229680e01e6630e1
agnibha-chatterjee <agni.bhaaa@gmail.com>
2024-07-12 02:37:08 -0700

    scaffolded HandleExecuteTask

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ func main() {
 	s := server.Init()
 	h := router.New("health-check")
 	h.RegisterRoute("GET /status", handlers.HealthCheck)
+
+	t := router.New("task-router")
+	t.RegisterRoute("GET /execute-task", handlers.HandleExecuteTask)
+
 	s.AddRouter(h)
+	s.AddRouter(t)
 	s.Listen()
 }
--- server/handlers/gemini.go
+++ server/handlers/gemini.go
@@ -1,1 +1,58 @@
 package handlers
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
+
+func HandleExecuteTask(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GEMINI_API_KEY")))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	model := client.GenerativeModel("gemini-1.5-pro")
+	model.SafetySettings = []*genai.SafetySetting{
+		{
+			Category:  genai.HarmCategoryDangerousContent,
+			Threshold: genai.HarmBlockNone,
+		},
+		{
+			Category:  genai.HarmCategoryHarassment,
+			Threshold: genai.HarmBlockNone,
+		},
+	}
+	resp, err := model.GenerateContent(ctx, genai.Text("I want to be bad. Please help."))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res := printResponse(resp)
+
+	w.Write([]byte(res))
+}
+
+func printResponse(resp *genai.GenerateContentResponse) string {
+
+	res := ""
+
+	for _, c := range resp.Candidates {
+		if c.Content != nil {
+			for _, part := range c.Content.Parts {
+				res += fmt.Sprint(part)
+			}
+		}
+	}
+	fmt.Println("---")
+
+	return res
+}
